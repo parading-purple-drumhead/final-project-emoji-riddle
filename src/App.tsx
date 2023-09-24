a8ade9b1258cc5d695905c61480f8e661be4c1b3
CMKCrew <32808558+eleon23@users.noreply.github.com>
2023-09-24 10:22:51 -0500

    added diagram

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import reactLogo from "./assets/react.svg";
+import architectureImage from "./assets/architectureImage.jpg";
 
 function App() {
   return (
@@ -7,6 +8,16 @@ function App() {
       <h1 className="title">Emoji Riddle with Friends</h1>
       <a href="https://react.dev" target="_blank">
         <img src={reactLogo} className="logo react" alt="React logo" />
+      </a>
+      <a
+        href="https://miro.com/app/board/uXjVMhiNYco=/?share_link_id=823805997381"
+        target="_blank"
+      >
+        <img
+          src={architectureImage}
+          alt="Architecture diagram"
+          style={{ width: "1500px", height: "1500px" }}
+        />
       </a>
     </div>
   );
