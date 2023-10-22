66a78f26f4de344cd00bbb6bc32abbee233224fa
CMKCrew <32808558+eleon23@users.noreply.github.com>
2023-10-22 11:17:06 -0500

    add architecture image

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import architectureImage from "./assets/main_architecture.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,6 +16,13 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <img
+          src={architectureImage}
+          className="architecture"
+          alt="Architecture"
+          width="1000px"
+          height="800px"
+        />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
@@ -29,7 +37,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
