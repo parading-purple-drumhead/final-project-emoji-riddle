68a9d46d967ac6cd2bddb25c9c30b3161b58e5aa
CMKCrew <32808558+eleon23@users.noreply.github.com>
2023-10-22 11:18:49 -0500

    cleaned up starter code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import architectureImage from "./assets/main_architecture.png";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <div>
@@ -16,6 +13,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        <h1>Emoji Riddle with Friends</h1>
         <img
           src={architectureImage}
           className="architecture"
@@ -24,18 +22,6 @@ function App() {
           height="800px"
         />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   );
 }
