8adceee98cd050e2d762c48ad507be49af99424d
Sudhanshu Basu Roy <sbasur2@uic.edu>
2023-10-26 15:08:56 -0500

    Router set up

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-component">
+      <nav className="navbar navbar-expand-lg bg-primary">
+        <div className="container">
+          <Link to="/" className="navbar-brand text-white">
+            Emoji Riddle With Friends
+          </Link>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
