bf90760685d0c8c5a550f1f0abda78e1bb2dd06e
Sudhanshu Basu Roy <sbasur2@uic.edu>
2023-10-31 22:46:52 -0500

    WIP: Implement game logic with firestore

--- src/pages/GameScreen.tsx
+++ src/pages/GameScreen.tsx
@@ -1,51 +1,119 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Guess from "../components/Guess";
 import Player from "../components/Player";
 import Phrase from "../components/Phrase";
 import { EmojiClickData } from "emoji-picker-react";
 import EmojiPicker from "emoji-picker-react";
+import { useLocation } from "react-router-dom";
+import {
+  arrayUnion,
+  doc,
+  getDoc,
+  onSnapshot,
+  updateDoc,
+} from "firebase/firestore";
+import { auth, db } from "../firebase/client";
+import { GuessProps, PlayerDetails } from "../utils/types";
 
 const GameScreen = () => {
+  const { state } = useLocation();
+  const { gameDetails } = state;
+  const [currentUserName, setCurrentUserName] = useState(null);
   const [roundStarted, setRoundStarted] = useState(false);
-  const [isTurn] = useState(true);
-  const [round] = useState(1);
+  const [roundCompleted, setRoundCompleted] = useState(false);
+  const [currentTurn, setCurrentTurn] = useState(null);
+  const [isTurn, setIsTurn] = useState(false);
+  const [round, setRound] = useState(1);
   const [phrases] = useState([
     "World is a stage",
     "Pants on fire",
     "Hot as hell",
   ]);
   const [phrase, setPhrase] = useState("");
-  const [players] = useState([
-    { name: "Player 1", isHost: true, isTurn: false },
-    { name: "Player 2", isHost: false, isTurn: false },
-    { name: "Player 3", isHost: false, isTurn: true },
-  ]);
+  const [players, setPlayers] = useState<PlayerDetails[]>([]);
   const [currentGuess, setCurrentGuess] = useState("");
-  const [guesses, setGuesses] = useState([
-    { name: "Player 1", guess: "Lorem ipsum dolor sit amet", correct: false },
-    { name: "Player 2", guess: "Lorem ipsum dolor sit amet", correct: false },
-    { name: "Player 3", guess: "Lorem ipsum dolor sit amet", correct: false },
-  ]);
+  const [guesses, setGuesses] = useState<GuessProps[]>([]);
 
-  const handleGuessClick = () => {
+  const fetchGameData = async () => {
+    const gameRef = doc(db, "games", gameDetails.name);
+    const gameData = (await getDoc(gameRef)).data();
+
+    const userRef = doc(db, "users", auth.currentUser!.uid);
+    const userData = (await getDoc(userRef)).data();
+
+    setPlayers(gameData?.players);
+    setCurrentUserName(userData?.name);
+  };
+
+  useEffect(() => {
+    fetchGameData();
+
+    const roundRef = doc(
+      db,
+      `games/${gameDetails.name}/rounds`,
+      `Round ${round}`
+    );
+
+    const unsubscribe = onSnapshot(roundRef, (doc) => {
+      const roundData = doc.data();
+
+      console.log(roundData);
+
+      setGuesses(roundData?.guesses);
+      setPhrase(roundData?.phrase);
+      setCurrentTurn(roundData?.turn);
+      setRoundStarted(roundData?.started);
+      setRoundCompleted(roundData?.completed);
+    });
+
+    console.log(currentTurn == auth.currentUser?.uid);
+
+    setIsTurn(currentTurn == auth.currentUser?.uid);
+
+    console.log(isTurn);
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleGuessClick = async () => {
+    const roundRef = doc(
+      db,
+      `games/${gameDetails.name}/rounds`,
+      `Round ${round}`
+    );
     if (currentGuess !== phrase) {
-      setGuesses([
-        ...guesses,
-        { name: "Player 1", guess: currentGuess, correct: false },
-      ]);
+      await updateDoc(roundRef, {
+        guesses: arrayUnion({
+          name: currentUserName,
+          guess: currentGuess,
+          correct: false,
+        }),
+      });
       setCurrentGuess("");
     } else {
-      setGuesses([
-        ...guesses,
-        { name: "Player 1", guess: "Player 1 guessed", correct: true },
-      ]);
+      await updateDoc(roundRef, {
+        guesses: arrayUnion({
+          name: currentUserName,
+          guess: `${currentUserName} guessed`,
+          correct: true,
+        }),
+      });
     }
   };
 
-  const handlePhraseClick = (e: React.FormEvent, phrase: string) => {
+  const handlePhraseClick = async (e: React.FormEvent, phrase: string) => {
     e.preventDefault();
-    setPhrase(phrase);
-    setRoundStarted(true);
+
+    const roundRef = doc(
+      db,
+      `games/${gameDetails.name}/rounds`,
+      `Round ${round}`
+    );
+
+    await updateDoc(roundRef, {
+      phrase: phrase,
+      started: true,
+    });
   };
 
   const [currentEmojiSelected, setCurrentEmoji] = useState([""]);
@@ -81,10 +149,9 @@ const GameScreen = () => {
           >
             {players.map((player, i) => (
               <Player
-                key={i}
-                uid={player.name}
-                isTurn={player.isTurn}
-                isHost={player.isHost}
+                uid={player.player}
+                isTurn={player.player == currentTurn}
+                isHost={i == 0}
               />
             ))}
           </div>
@@ -103,7 +170,7 @@ const GameScreen = () => {
                   alignItems: "center",
                 }}
               >
-                {!isTurn && (
+                {!isTurn && !roundStarted && (
                   <p
                     className="text-white text-center"
                     style={{ fontSize: "2rem" }}
@@ -112,7 +179,7 @@ const GameScreen = () => {
                   </p>
                 )}
 
-                {isTurn && (
+                {isTurn && !roundStarted && (
                   <div className="phrase-picker">
                     <h2 className="text-center text-white">Pick a Phrase</h2>
                     <div className="row mt-4">
@@ -144,7 +211,7 @@ const GameScreen = () => {
                   height: "80vh",
                 }}
               >
-                {isTurn && (
+                {isTurn && roundStarted && (
                   <div>
                     <div
                       className="row bg-primary"
@@ -171,28 +238,41 @@ const GameScreen = () => {
                     </div>
                   </div>
                 )}
-                {!isTurn && (
-                  <div
-                    className="row py-4"
-                    style={{ borderTop: "1px solid #ddd" }}
-                  >
-                    <div className="col-10">
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Type your guess..."
-                        value={currentGuess}
-                        onChange={(e) => setCurrentGuess(e.target.value)}
-                      />
+                {!isTurn && roundStarted && (
+                  <div>
+                    <div
+                      className="row bg-primary"
+                      id="emoji-display"
+                      style={{
+                        height: "70vh",
+                        textAlign: "center",
+                        color: "white",
+                      }}
+                    >
+                      <h3>Emoji display</h3>
                     </div>
-                    <div className="col-2">
-                      <button
-                        className="btn btn-success btn-small"
-                        style={{ width: "100%" }}
-                        onClick={handleGuessClick}
-                      >
-                        Guess
-                      </button>
+                    <div
+                      className="row py-4"
+                      style={{ borderTop: "1px solid #ddd" }}
+                    >
+                      <div className="col-10">
+                        <input
+                          type="email"
+                          className="form-control"
+                          placeholder="Type your guess..."
+                          value={currentGuess}
+                          onChange={(e) => setCurrentGuess(e.target.value)}
+                        />
+                      </div>
+                      <div className="col-2">
+                        <button
+                          className="btn btn-success btn-small"
+                          style={{ width: "100%" }}
+                          onClick={handleGuessClick}
+                        >
+                          Guess
+                        </button>
+                      </div>
                     </div>
                   </div>
                 )}
