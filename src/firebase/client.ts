d9206d07638905281eec929a946ce1a591f5ad9f
Lydia Tse <lydia@MacBook-Pro-5.local>
2023-12-01 00:48:48 -0600

    Updated firestore credentials

--- src/firebase/client.ts
+++ src/firebase/client.ts
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB0r2yar_2x00Rl87l_rf759zK8YOQl4MY",
-  authDomain: "emoji-riddle-with-friends.firebaseapp.com",
-  projectId: "emoji-riddle-with-friends",
-  storageBucket: "emoji-riddle-with-friends.appspot.com",
-  messagingSenderId: "526900602444",
-  appId: "1:526900602444:web:9fe9522c0981a061f5f6e9",
+  apiKey: "AIzaSyB41-DMy-o3iOzKqAZj3SPwpUPAJl0BSbA",
+  authDomain: "pictoverse-puzzle.firebaseapp.com",
+  projectId: "pictoverse-puzzle",
+  storageBucket: "pictoverse-puzzle.appspot.com",
+  messagingSenderId: "737641848080",
+  appId: "1:737641848080:web:e51aa81afc759fce5d9995",
 };
 
 // Initialize Firebase
